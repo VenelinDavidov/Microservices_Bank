ed9760163ff69bfc01d44f91c2357050eddbc0de
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2025-08-03 15:18:11 +0300

    Spring Cloud Config
    Spring Cloud Configurations Managements in Microservices

---
+++ section6/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -0,0 +1,16 @@
+package com.eazybytes.accounts.service;
+
+import com.eazybytes.accounts.dto.CustomerRequest;
+
+public interface IAccountsService {
+
+
+    void createAccount(CustomerRequest customerRequest);
+
+    boolean updateAccount (CustomerRequest customerRequest);
+
+    CustomerRequest fetchAccountDetails(String mobileNumber);
+
+    boolean deleteAccount(String mobileNumber);
+
+}
