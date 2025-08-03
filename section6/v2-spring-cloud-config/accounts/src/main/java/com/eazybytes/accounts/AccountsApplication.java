d4ca64a92895b52d7ef9af124eb2ddfa110b987c
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2025-08-03 15:09:50 +0300

    Spring Cloud Config
    Configurations Managements in Microservices in my app

---
+++ section6/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,47 @@
+package com.eazybytes.accounts;
+
+import com.eazybytes.accounts.dto.AccountsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.accounts.controller") })
+@EnableJpaRepositories("com.eazybytes.accounts.repository")
+@EntityScan("com.eazybytes.accounts.model")*/
+
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice Rest API Documentation",
+				description = "EasyBank Accounts for microservice Rest API",
+				version = "v1",
+				contact = @Contact(
+						name = "Venelin Davidov",
+						email = "venelin.davidov@gmail.com",
+						url = "https://github.com/venelin-davidov"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/venelin-davidov"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EasyBank Accounts for microservice Rest API",
+				url = "https://github.com/venelin-davidov"
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
