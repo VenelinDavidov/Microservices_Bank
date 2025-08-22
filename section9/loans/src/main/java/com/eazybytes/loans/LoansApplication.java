44edaa14aa920c56cc180d217960b7a65cee6c09
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2025-08-22 17:04:02 +0300

    Create API Gateway Server
    new update with Edge server and Eureka server

---
+++ section9/loans/src/main/java/com/eazybytes/loans/LoansApplication.java
@@ -0,0 +1,45 @@
+package com.eazybytes.loans;
+
+import com.eazybytes.loans.dto.LoansContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+/*@ComponentScans({ @ComponentScan("com.eazybytes.loans.controller") })
+@EnableJpaRepositories("com.eazybytes.loans.repository")
+@EntityScan("com.eazybytes.loans.model")*/
+@EnableConfigurationProperties(value = {LoansContactInfoDto.class})
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Loans microservice REST API Documentation",
+                version = "1.0",
+                contact = @Contact(
+                        name = "John Doe",
+                        url = "http://www.eazybytes.com",
+                        email = "Fm7yO@example.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "k Loans microservice REST API Documentation",
+                url = "https://www.eazybytes.com/swagger-ui.html"
+        )
+)
+public class LoansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (LoansApplication.class, args);
+    }
+
+}
