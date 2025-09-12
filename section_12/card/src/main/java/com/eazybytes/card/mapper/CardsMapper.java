e0b3ad81b372ae5b343d912804061a71edfd0e55
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2025-09-13 00:16:54 +0300

    Microservice Security
    Use OAUTH2 and Open ID Connect

---
+++ section_12/card/src/main/java/com/eazybytes/card/mapper/CardsMapper.java
@@ -0,0 +1,32 @@
+package com.eazybytes.card.mapper;
+
+import com.eazybytes.card.dto.CardsDto;
+import com.eazybytes.card.entity.Cards;
+
+public class CardsMapper {
+
+    public  static CardsDto mapToCardsDto(Cards cards, CardsDto cardsDto) {
+
+        cardsDto.setMobileNumber(cards.getMobileNumber());
+        cardsDto.setCardNumber(cards.getCardNumber());
+        cardsDto.setCardType(cards.getCardType());
+        cardsDto.setTotalLimit(cards.getTotalLimit());
+        cardsDto.setAmountUsed(cards.getAmountUsed());
+        cardsDto.setAvailableAmount(cards.getAvailableAmount());
+
+        return cardsDto;
+    }
+
+
+    public static Cards mapToCards(CardsDto cardsDto, Cards cards) {
+
+        cards.setCardNumber(cardsDto.getCardNumber());
+        cards.setCardType(cardsDto.getCardType());
+        cards.setMobileNumber(cardsDto.getMobileNumber());
+        cards.setTotalLimit(cardsDto.getTotalLimit());
+        cards.setAvailableAmount(cardsDto.getAvailableAmount());
+        cards.setAmountUsed(cardsDto.getAmountUsed());
+
+        return cards;
+    }
+}
